f9c48f9cf80b8ced419bb8b03206ca64e06daeb1
Vadim <VadimPodvouskiy@users.noreply.github.com>
2019-11-19 14:32:30 +0300

    Project

---
+++ Lift.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Lift.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ Lift/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Lift.Models;
+using Lift.Service.User;
+
+namespace Lift.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("MenuInterface");
+        }
+
+        public ViewResult LiftInterface()
+        {
+            return View();
+        }
+
+        public ViewResult ExitInterface()
+        {
+            return View();
+        }
+    }
+}
